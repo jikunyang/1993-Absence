282087883019a0e4074e993f7bf42f1c6a33441c
Kunkun <jikunyang03@gmail.com>
2024-12-21 21:21:17 +1300

    shader updated

--- js/canvas.js
+++ js/canvas.js
@@ -11,27 +11,47 @@ const loadCanvas = () => {
     // Create a new GlslCanvas instance
     const sandbox = new GlslCanvas(canvas);
 
-    
     // Load the shader code
     sandbox.load(fragTD);
-    // Create a WebGL texture with GL_NEAREST filtering
 
     // Set the texture to GlslCanvas
     sandbox.setUniform("u_texture", "https://cdn.prod.website-files.com/6750d2ccd60d9947409d2c73/6765449f2baf12bacae294dc_fwafaw%201-min.jpg");
     sandbox.setUniform("u_resolution", [canvas.width, canvas.height]);
 
+    // Web Audio API setup
+    const audio = new Audio("https://od.lk/s/NDhfNjI0MTY2MTBf/spotifydown.com%20-%20%E9%9B%AA%E3%81%AE%E8%A9%A0%E5%A5%B3.mp3");
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const analyser = audioContext.createAnalyser();
+    const source = audioContext.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
 
-    // Mouse move event listener
-    /**canvas.addEventListener('mousemove', (e) => {
-        const rect = canvas.getBoundingClientRect();
-        const mouseX = e.clientX - rect.left;
-        const mouseY = e.clientY - rect.top;
-        sandbox.setUniform("u_mouse", [mouseX, mouseY]);
-    });**/
+    analyser.fftSize = 256;
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+    // Update audio data and send to the shader
+    const updateAudioData = () => {
+        analyser.getByteFrequencyData(dataArray);
+
+        // Calculate audio intensity (e.g., average of frequency data)
+        const audioIntensity = dataArray.reduce((sum, value) => sum + value, 0) / dataArray.length;
+
+        // Send intensity to the shader
+        sandbox.setUniform("u_audioIntensity", audioIntensity / 256); // Normalize to [0, 1]
+    };
+
+    // Rendering loop
+    const render = () => {
+        updateAudioData();
+        requestAnimationFrame(render);
+    };
+
+    // Start audio and rendering loop
+    audio.play();
+    render();
+
     console.log(fragTD);
     console.log("canvas");
-
-
-
 };
+
 export default loadCanvas;
